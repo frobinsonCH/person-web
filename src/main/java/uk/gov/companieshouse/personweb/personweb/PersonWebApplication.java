34336d9a216477845ab979d618e3f4533eea9a3b
Frank Robinson <frobinson@companieshouse.gov.uk>
2021-06-22 14:23:40 +0100

    init commit

---
+++ src/main/java/uk/gov/companieshouse/personweb/personweb/PersonWebApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.personweb.personweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/personweb/personweb/PersonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.personweb.personweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
